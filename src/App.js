0ed8e4ea9dce0a0b797252164bd6286cb62800c9
Piyush Mehta <me@piyushmehta.com>
2019-03-03 12:14:47 +0530

    Setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// function App() {
+//   const [todos, setTodos] = useState([
+//     { text: "Learn about React" },
+//     { text: "Meet friend for lunch" },
+//     { text: "Build really cool todo app" }
+//   ]);
+
+//   // we'll render our todos here ...
+//   // return <div></div>
+// }
 
 class App extends Component {
   render() {
